e0cbc780a2ec978acb43a117376a5a20e29a8cfe
james-skills <jameschan0088@gmail.com>
2026-09-17 13:09:43 +0800

    add Product controller and simple view

---
+++ examples/helix-basic-unicorn/src/Foundation/FieldRendering/website/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using Sitecore.Mvc.Controllers;
+
+namespace BasicCompany.Feature.Product.Controllers
+{
+    public class ProductController : SitecoreController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
